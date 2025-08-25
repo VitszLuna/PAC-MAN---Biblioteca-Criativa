e43a844d04f65b400f3c878edd31a1a91b221c9d
Vitoria Luna! <isabeli.vitoria.luna@gmail.com>
2025-08-25 10:47:41 -0300

    Delete fazendofuncionar.js

--- fazendofuncionar.js
+++
@@ -1,9 +0,0 @@
-  function salvarNome() {
-      const nome = document.getElementById("nome").value;
-      if (nome.trim() !== "") {
-        localStorage.setItem("nomeUsuario", nome);
-        window.location.href = "home.html";
-      } else {
-        alert("Por favor, digite seu nome.");
-      }
-    }
